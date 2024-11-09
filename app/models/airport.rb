99ebf2e03c6158e028e60ad5d31bfdfd40f122c5
Christian <christianvillere@gmail.com>
2024-11-08 20:00:48 -0800

    Final commit before moving macs

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,4 @@
 class Airport < ApplicationRecord
-  has_many_and_belongs_to_many :departing_flights, foreign_key: :departing_flight_id, class_name: "Flight"
-  has_many_and_belongs_to_many :arriving_flights, foreign_key: :arriving_flight_id, class_name: "Flight"
+  has_many :departing_flights, foreign_key: :departure_airport_id, class_name: "Flight"
+  has_many :arriving_flights, foreign_key: :arrival_airport_id, class_name: "Flight"
 end
