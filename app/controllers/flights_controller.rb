7ba9a94540de8946d278715d4c83815cb256f910
Christian <christianvillere@gmail.com>
2024-11-10 21:47:33 -0800

    Add flight date selection possibility to index.html.erb

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,11 +1,17 @@
 class FlightsController < ApplicationController
 
+  before_action :flight_options, only: [:index]
+
   def index
     @flights = Flight.all
   end
 
 
   private
+
+  def flight_options
+    @flight_options = Flight.all.map { |u| [u.start.strftime("%m/%d/%Y"), u.id] } 
+  end
 
   def flight_params
     params.require(:flight).permit(:departure_airport, :arrival_airport, :start, :flight_duration)
