efb9385bc6eefe5e56f3eb50f64e08a41e588f4d
Christian <christianvillere@gmail.com>
2024-12-24 22:12:57 -0800

    Correct new action in BookingsController

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,25 +1,32 @@
 class BookingsController < ApplicationController
 
   def new
-    #@num_passengers = params[:num_passengers].to_i
-    #@flight_id = params[:flight_id]
-    #@booking = Booking.new
 
-    @booking = Booking.new(booking_params)
+    @flight_id = params[:flight_id]
+    @num_passengers = params[:num_passengers].to_i
+    @booking = Booking.new()
+
+    
+  end
+
+  def create
+
+    @num_passengers.times { @booking.passengers.build(booking_params) } if @num_passengers > 0
 
     if @booking.save
-      redirect_to flights_path, notice: "Booking successful"
+      redirect_to flights_path, notice: "Successfully selected flight"
     else
-      flash.now[:alert] = "There was an error creating your booking."
+      flash.now[:alert] = "There was an error selecting your flight."
       render status: :unprocessable_entity
     end
-    
+
+
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:flight_id)
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
   end
 
 end
