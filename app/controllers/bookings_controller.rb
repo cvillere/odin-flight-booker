595b18a1f9c0e3987d14b197789a5f4900b4318a
Christian <christianvillere@gmail.com>
2025-01-02 11:01:58 -0800

    Add changes to BookingsController and _passenger_form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,13 +3,16 @@ class BookingsController < ApplicationController
   def new
 
     @flight_id = params[:flight_id]
+    @flights = Flight.find(@flight_id)
     @num_passengers = params[:num_passengers].to_i
     @booking = Booking.new()
-
+    #@num_passengers.times { @booking.passengers.build(booking_params) } if @num_passengers > 0
     
   end
 
   def create
+
+    #@flights = Flight.find(params[:flight_id])
 
     @num_passengers.times { @booking.passengers.build(booking_params) } if @num_passengers > 0
 
