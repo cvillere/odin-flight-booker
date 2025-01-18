29504af4b0a4ad6a9e45427e80142c61b8d51532
Christian <christianvillere@gmail.com>
2025-01-18 13:04:55 -0800

    Change fields_for form to get passenger info.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,29 +1,31 @@
 class BookingsController < ApplicationController
 
+  @booking = Booking.new
+
   def new
 
     @flight_id = params[:flight_id]
     @flights = Flight.find(@flight_id)
     @num_passengers = params[:num_passengers].to_i
     @booking = Booking.new
-    #@num_passengers.times { @booking.passengers.build } if @num_passengers > 0
+    @num_passengers.times { @booking.passengers.build } if @num_passengers > 0
     
   end
 
   def create
 
+    #@flight_id = params[:flight_id]
     #@flights = Flight.find(params[:flight_id])
-    #@booking = Booking.new(booking_params)
-    #byebug
-    @num_passengers = params[:num_passengers].to_i
+    #@num_passengers = params[:num_passengers].to_i
     #@booking = Booking.new
-    @booking = @booking.passengers.build(booking_params)
+    @booking = Booking.new(booking_params)
 
     if @booking.save
       redirect_to flights_path, notice: "Successfully selected flight"
     else
+      puts @booking.errors.full_messages
       flash.now[:alert] = "There was an error selecting your flight."
-      render status: :unprocessable_entity
+      render partial: "passenger_form", status: :unprocessable_entity
     end
 
   end
