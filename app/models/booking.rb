7e5cc100fa616f9802bd7d7df1b47837f352861f
Christian <christianvillere@gmail.com>
2025-01-26 21:05:29 -0800

    Update passenger model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,4 @@
 class Booking < ApplicationRecord
-  belongs_to :departing_flights, foreign_key: :departure_airport_id, class_name: "Flight"
-  belongs_to :arriving_flights, foreign_key: :arrival_airport_id, class_name: "Flight"
 
   has_many :passengers
   accepts_nested_attributes_for :passengers
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,8 +1,6 @@
 class Flight < ApplicationRecord
-  belongs_to :arrival_airport, foreign_key: :arrival_airport_id, class_name: "Airport"
-  belongs_to :departure_airport, foreign_key: :departure_airport_id, class_name: "Airport"
-
-  has_many :bookings
+  belongs_to :arrival_airport, foreign_key: "arrival_airport_id", class_name: "Airport"
+  belongs_to :departure_airport, foreign_key: "departure_airport_id", class_name: "Airport"
 
   def self.start_formatted
     all.map { |flight| [flight.start.strftime("%m/%d/%Y"), flight.id] }
@@ -11,7 +9,5 @@ class Flight < ApplicationRecord
   scope :results, -> (departure_airport, arrival_airport, start) {
     where("departure_airport_id = ? AND arrival_airport_id = ? AND start BETWEEN ? AND ?", departure_airport, arrival_airport, start.begin, start.end)
   }
-
-
 
 end
